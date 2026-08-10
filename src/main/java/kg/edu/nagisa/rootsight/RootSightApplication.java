cdc0b5ba2009feee6e05d2efc5968b2d2d4602cf
fangzhou <3231780773@qq.com>
2026-08-10 18:05:48 +0800

    初始化

---
+++ src/main/java/kg/edu/nagisa/rootsight/RootSightApplication.java
@@ -0,0 +1,13 @@
+package kg.edu.nagisa.rootsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RootSightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RootSightApplication.class, args);
+    }
+
+}
